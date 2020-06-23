38af03563a816a5fd256f32563029f8784cbd100
Belleisa <47032848+Belleisa@users.noreply.github.com>
2020-06-23 16:36:04 -0300

    questions

--- src/app/questions.service.ts
+++ src/app/questions.service.ts
@@ -3,40 +3,18 @@ import { HttpClient } from '@angular/common/http';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { QuestionsComponent } from './questions/questions.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionsService {
 
-  listRef: AngularFireList<any>;
-  list: Observable<any[]>;
-
-  constructor(private db: AngularFireDatabase) { 
-
-    this.listRef = db.list('questions');
-    this.list = this.listRef.snapshotChanges().pipe(
-      map(changes =>
-        changes.map(c => ({ key: c.payload.key, ...c.payload.val()}))
-      )
-    );
- 
-  }
- 
-  add(q: string, a: string, a1: string, a2: string, a3: string, a4: string) {
-    this.listRef.push(
-      {
-        question: q,
-        answer: a,
-        alternativa1: a1,
-        alternativa2: a2,
-        alternativa3: a3,
-        alternativa4: a4,
-      }
-    ); 
+  constructor(public questions: QuestionsComponent){
+    
   }
 
-  delete(key: string) {
-    this.listRef.remove(key);
+  getQuestions(){
+    return this.questions.add;
   }
 }
--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { QuestionsService } from '../questions.service';
 
 @Component({
   selector: 'app-questions',
@@ -11,13 +10,38 @@ import { QuestionsService } from '../questions.service';
 })
 export class QuestionsComponent implements OnInit {
 
-  constructor(public questions: QuestionsService) { 
-    
+  listRef: AngularFireList<any>;
+  list: Observable<any[]>;
+
+  constructor(private db: AngularFireDatabase) { 
+
+    this.listRef = db.list('questions');
+    this.list = this.listRef.snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val()}))
+      )
+    );
+ 
+  }
+ 
+  add(q: string, a: string, a1: string, a2: string, a3: string, a4: string) {
+    this.listRef.push(
+      {
+        question: q,
+        answer: a,
+        alternativa1: a1,
+        alternativa2: a2,
+        alternativa3: a3,
+        alternativa4: a4,
+      }
+    ); 
   }
 
-  getQuestions(){
-    
+  delete(key: string) {
+    this.listRef.remove(key);
   }
+
+  
 
   ngOnInit(): void {
   }
