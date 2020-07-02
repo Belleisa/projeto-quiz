3f4f3ea9b35dfe201307723da8e55bcc5fe7dc7d
Lara Flausino <laraeloisaflausino@gmail.com>
2020-07-02 19:21:12 -0300

    questions quiz

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { FirebaseService, Question } from '../services/firebase.service';
 
 @Component({
   selector: 'app-quiz',
@@ -10,9 +11,66 @@ import { map } from 'rxjs/operators';
 })
 export class QuizComponent implements OnInit {
 
-  constructor() { }
+  user = '';
+  questions = [];
+  answers: { [key in string]: string } = {};
 
-  ngOnInit(): void {
+  points = 0;
+  answered = false;
+  corrects = 0;
+
+  constructor(private firebaseService: FirebaseService) { }
+
+  shuffle(array) {
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
   }
 
+  saveQuestionAwnser(question, answered) {
+    this.answers[question] = answered;
+  }
+
+  sendAnswers() {
+
+    let points = 0;
+    let correctAnswer = 0;
+
+    this.questions.forEach((question: Question, index: number) => {
+      if (this.answers[index] && this.answers?.[index] === question.answer) {
+        points += 10;
+        correctAnswer += 1;
+      }
+    });
+
+    this.points = points;
+    this.corrects = correctAnswer;
+    this.answered = true;
+
+    this.firebaseService.addAnswer({
+      usuario: this.user,
+      points,
+      correct_answer: correctAnswer,
+    });
+  }
+
+  setUser(user: string) {
+    this.user = user;
+  }
+
+  ngOnInit(): void {
+    this.firebaseService.getQuestions().subscribe((questions) => {
+      this.questions = this.shuffle(questions);
+    });
+  }
 }
